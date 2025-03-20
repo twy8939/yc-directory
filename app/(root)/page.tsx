1ad337db0786f412ae377739781888ecb48df66a
twy8939 <twy8939@naver.com>
2025-03-20 21:44:48 +0900

    feat: Add GitHub OAuth authentication

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-2xl">Home</h1>
+    </>
+  );
+}
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,55 @@
+import { auth, signIn } from "@/auth";
+import { signOut } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = async () => {
+  const session = await auth();
+
+  return (
+    <div className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
+
+        <div className="flex items-center gap-5">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+
+              <form
+                action={async () => {
+                  "use server";
+
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">Logout</button>
+              </form>
+
+              <Link href={`/user/${session?.user?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
