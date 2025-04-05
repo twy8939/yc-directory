676fcc846226edc02f3119f96ce6fcbd7e692e24
twy8939 <twy8939@naver.com>
2025-04-05 10:57:23 +0900

    feat : Add startup by id query

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -12,5 +12,22 @@ export const STARTUPS_QUERY =
   views,
   description,
   category,
-  image
+  image,
+  pitch,
+}`);
+
+export const STARTUP_BY_ID_QUERY =
+  defineQuery(`*[_type == "startup" && _id == $id][0]{
+  _id,
+  title,
+  slug,
+  _createAt,
+  author -> {
+    _id, name, username, image, bio
+  },
+  views,
+  description,
+  category,
+  image,
+  pitch
 }`);
