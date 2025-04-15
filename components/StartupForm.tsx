a5d65fec02f8f7df093b3d43830c7c6d636f7664
twy8939 <twy8939@naver.com>
2025-04-16 00:09:49 +0900

    feat: add pitch validation and submit button to startup form

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -1,15 +1,53 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useActionState, useState } from "react";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 import MDEditor from "@uiw/react-md-editor";
 import { Button } from "./ui/button";
+import { Send } from "lucide-react";
+import { formSchema } from "@/lib/vaildation";
+import { z } from "zod";
 
 const StartupForm = () => {
   const [errors, setErrors] = useState<Record<string, string>>({});
 
-  const [pitch, setPitch] = useState("**Hello world!!!**");
+  const [pitch, setPitch] = useState("");
+
+  const handleFormSubmit = async (prevState: any, formData: FormData) => {
+    try {
+      const formValues = {
+        title: formData.get("title") as string,
+        description: formData.get("description") as string,
+        category: formData.get("category") as string,
+        link: formData.get("link") as string,
+        pitch,
+      };
+
+      await formSchema.parseAsync(formValues);
+
+      //   const result = await createDiffieHellman(prevState, formData, pitch);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        const fieldErrors = error.flatten().fieldErrors;
+
+        setErrors(fieldErrors as unknown as Record<string, string>);
+
+        return { ...prevState, error: "Validation failed", status: "ERROR" };
+      }
+
+      return {
+        ...prevState,
+        error: "An unexpected error has occurred",
+        status: "ERROR",
+      };
+    }
+  };
+
+  const [state, formAction, isPending] = useActionState(handleFormSubmit, {
+    error: "",
+    status: "INITIAL",
+  });
 
   return (
     <form action={() => {}} className="startup-form">
@@ -90,6 +128,11 @@ const StartupForm = () => {
         />
         {errors.pitch && <p className="startup-form_error">{errors.pitch}</p>}
       </div>
+
+      <Button type="submit" className="startup-form_btn" disabled={isPending}>
+        {isPending ? "Submitting..." : "Submit Your Pitch"}
+        <Send className="size-6" />
+      </Button>
     </form>
   );
 };
