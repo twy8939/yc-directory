3edb62bfcffac2a96f3245430b43fff1b52cad47
twy8939 <twy8939@naver.com>
2025-04-22 20:39:26 +0900

    feat: add user profile page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -30,7 +30,7 @@ const Navbar = async () => {
                 <button type="submit">Logout</button>
               </form>
 
-              <Link href={`/user/${session?.user?.id}`}>
+              <Link href={`/user/${session?.id}`}>
                 <span>{session?.user?.name}</span>
               </Link>
             </>
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -49,3 +49,15 @@ export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`
     bio
 }
 `);
+
+export const AUTHOR_BY_ID_QUERY = defineQuery(`
+  *[_type == "author" && _id == $id][0]{
+      _id,
+      id,
+      name,
+      username,
+      email,
+      image,
+      bio
+  }
+  `);
