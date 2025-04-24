a6b79426938ce542dbffeea2d0e18dc9ec57b4fb
twy8939 <twy8939@naver.com>
2025-04-24 17:58:30 +0900

    feat: update startup views to increment on view

--- components/View.tsx
+++ components/View.tsx
@@ -1,22 +1,21 @@
-import React from "react";
-import Ping from "./Ping";
+import Ping from "@/components/Ping";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
 import { writeClient } from "@/sanity/lib/write-client";
-import { after } from "next/server";
+import { unstable_after as after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
 
-  // after(
-  //   async () =>
-  //     await writeClient
-  //       .patch(id)
-  //       .set({ views: totalViews + 1 })
-  //       .commit()
-  // );
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
 
   return (
     <div className="view-container">
@@ -25,10 +24,9 @@ const View = async ({ id }: { id: string }) => {
       </div>
 
       <p className="view-text">
-        <span className="font-black">Views: {totalViews} </span>
+        <span className="font-black">Views: {totalViews}</span>
       </p>
     </div>
   );
 };
-
 export default View;
